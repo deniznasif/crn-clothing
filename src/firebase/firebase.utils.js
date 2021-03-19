cf521180de51ba1f868ac4bd6bfa5ce2b5acb85f
deniznasif <deniznasif@hotmail.co.uk>
2021-03-19 09:16:50 +0000

    WIP

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyD5YnprK097DvWgkv8mfAcDlH8mr3pP5Mw",
+    authDomain: "crwn-db-b352d.firebaseapp.com",
+    projectId: "crwn-db-b352d",
+    storageBucket: "crwn-db-b352d.appspot.com",
+    messagingSenderId: "541697732084",
+    appId: "1:541697732084:web:02ac72ee7379c2eb7a1da1"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
