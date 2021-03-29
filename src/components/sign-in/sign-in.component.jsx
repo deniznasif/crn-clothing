11213ea85a80c69315c3fa79bfac99bd8b6ce6f9
deniznasif <deniznasif@hotmail.co.uk>
2021-03-29 22:35:28 +0100

    wip

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,12 +45,13 @@ class SignIn extends React.Component {
                     ype="password" value={this.state.password} 
                     handleChange ={ this.handleChange } 
                     label="password" 
-                    required />
-
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        {' '}Sign in with Google{' '}
-                    </CustomButton>
+                    required /> 
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            {' '}Sign in with Google{' '}
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         )
