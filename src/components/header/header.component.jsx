12df9aebdc0df07759667d8e83b80c0192b09d31
@DENIZ.NASIF.DDBREMEDY.CO.UK <deniz.nasif@ddbremedy.co.uk>
2021-06-02 08:27:14 +0100

    created cart dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
@@ -17,13 +19,14 @@ const Header = ({ currentUser }) => (
             <Link className="option" to="/shop">
                 CONTACT
             </Link>
-            { 
-                currentUser ? 
-                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> 
-                : 
-                <Link className="option" to="/signin">SIGN IN</Link> 
-            }
+            { currentUser ? (
+                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+            ) : (
+                <Link className="option" to="/signin">SIGN IN</Link>
+            )}
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
